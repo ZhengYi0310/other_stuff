4b7f61254ef2c9d369bbd060b27d47fecdceb945
Yi Zheng <hczhengcq@gmail.com>
2017-08-11 17:46:23 -0700

    add some functions

--- deeprlhw/hw1/run_imitation.py
+++ deeprlhw/hw1/run_imitation.py
@@ -9,6 +9,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Lambda, Dropout
 from keras import initializers
 from keras import optimizers
+from keras import callbacks
 
 def run_export_policy_gather_data(config):
     print('loading and building expert policy')
@@ -18,7 +19,6 @@ def run_export_policy_gather_data(config):
     with tf.Session():
         tf_util.initialize()
 
-        import gym
         env = gym.make(config['env'])
         max_steps = config['max_timesteps'] or env.spec.timestep_limit
 
@@ -60,7 +60,7 @@ def run_export_policy_gather_data(config):
 
 def train_NN_policy(config, data, env):
 
-    expert_data = pickle.load(open(config['expoert_data_path']))
+    expert_data = pickle.load(open(config['expert_data_path']))
 
     # Construct the models
     model = Sequential()
@@ -76,5 +76,57 @@ def train_NN_policy(config, data, env):
 
     opt = optimizers.Adagrad(lr=config['learning_rate'], epsilon=config['epsilon'], decay=config['decay'])
     model.compile(optimizer=opt, loss='mean_squared_error', metrics=['mse', 'accuracy'])
+
+    early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=2)
+
+    model.fit(expert_data['observations'], expert_data['actions'].reshape(-1, env.action_space.shape[0]), validation_split=0.2,
+              batch_size=40, epochs=100, verbose=2, shuffle=True, callbacks=[early_stopping])
+
+    model.save('behavior_cloning_model.h5')
+
+    return model
+
+def test_NN_policy(model, config, env):
+    max_steps = config['max_timesteps'] or env.spec.timestep_limit
+
+    returns = []
+    observations = []
+    actions = []
+    steps_list = []
+    for i in tqdm(range(config['num_rollouts'])):
+        print('rollout: ', i)
+        obs = env.reset()
+        done = False
+        totalr = 0.
+        steps = 0
+        while not done:
+            action = model.predict(obs[None, :])
+            observations.append(obs)
+            actions.append(action)
+            obs, r, done, _ = env.step(action)
+            totalr += r
+            steps += 1
+            if config['do_rendering']:
+                env.render()
+            if steps % 100 == 0: print("%i/%i" % (steps, max_steps))
+            if steps >= max_steps:
+                break
+        returns.append(totalr)
+        steps_list.append(steps)
+
+    print('returns', returns)
+    print('mean return', np.mean(returns))
+    print('std of return', np.std(returns))
+
+    policy_data = {'observations': np.array(observations),
+                   'actions': np.array(actions),
+                   'returns': np.array(returns),
+                   'steps': np.array(steps_list)}
+
+
+    pickle.dump(policy_data, open(config['policy_data_path'], 'wb'))
+    return policy_data
+
+
 
 
